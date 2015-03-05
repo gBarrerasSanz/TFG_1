d0b26909b1624947767be52738033f6eb56b6859
Gabi <gbarreras93@gmail.com>
2015-03-05 19:34:23 +0100

    fix2

---
+++ GuiaTV/src/main/java/pack1/Main.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class Main {
+	public static void main(String args[]){
+		System.out.println("Hello World");
+		System.out.println("Good bye");
+		System.out.println("AAA");
+		System.out.println("BBB");
+		System.out.println("CCC");
+		System.out.println("DDD");
+	}
+}
---
+++ GuiaTV/src/test/java/test/test1.java
@@ -0,0 +1,13 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test1 {
+	@Test
+	public void test1_1() throws Exception {
+		assertEquals(1+1,2);
+	}
+		
+}
