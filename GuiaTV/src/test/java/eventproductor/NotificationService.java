caea8c9cfbb2db765d656274385a5f6ab8233e66
Gabi <gbarreras93@gmail.com>
2015-03-06 20:10:37 +0100

    Haciendo el test del MQTT

--- GuiaTV/src/test/java/eventproductor/NotificationService.java
+++ GuiaTV/src/test/java/eventproductor/NotificationService.java
@@ -1,64 +1,74 @@
 package eventproductor;
 
-import org.springframework.cglib.proxy.CallbackGenerator.Context;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 
 public class NotificationService {
-	 
-    private String broker = "tcp://{rabbitmq_server}:1883";
-    private int qos = 0;
- 
-    private MqttCallBack mqttCallBack;
-    private Context context;
- 
-    private MqttClient notificationClient = null;
- 
-    private MemoryPersistence persistence = new MemoryPersistence();
- 
-    public NotificationService(Context context, MqttCallBack mqttCallBack) {
- 
-        this.context = context;
-        this.mqttCallBack = mqttCallBack;
-    }
- 
-    public void start() throws MqttException{
- 
-        this.notificationClient =
-                new MqttClient(
-                        this.broker,
-                        MqttClient.generateClientId(),
-                        this.persistence);
- 
-        MqttConnectOptions connOpts = new MqttConnectOptions();
-        connOpts.setUserName("user");
-        connOpts.setPassword("user".toCharArray());
-        connOpts.setCleanSession(true);
- 
-        this.notificationClient.setCallback(this.mqttCallBack);
- 
-        this.notificationClient.connect(connOpts);
- 
-        if(!this.notificationClient.isConnected()){
- 
-            Log.e("MQTT_ERROR", "Error Connecting on notification server.");
-            return;
-        }
- 
-        this.notificationClient.subscribe("amq.topic.*", this.qos);
- 
-        Log.i("MQTT_ERROR", "Connected");
-    }
- 
-    public void disconnect(){
- 
-        try{
- 
-            this.notificationClient.disconnect();
-        }catch(MqttException me){
- 
-            Log.e(
-                    "MQTT_ERROR",
-                    "Error disconnecting connection from notification server.\n" + me.getMessage());
-        }
-    }
- 
+
+	String clientId     = "RTNClient";
+	MqttDeliveryToken token;
+	int qos = 2;
+
+	MqttClient mqttclient;
+	String username;
+	char[] password;
+	String broker;
+
+	public NotificationService(){
+		this.broker = "http://localhost:1883";
+		this.username = "guest";
+		this.password = "guest".toCharArray();
+		MemoryPersistence persistence = new MemoryPersistence();
+
+		try {
+			System.out.print(broker);
+			MqttConnectOptions connOpts = new MqttConnectOptions();
+			connOpts.setCleanSession(true);
+			connOpts.setUserName(username);
+			connOpts.setPassword(password);
+			mqttclient = new MqttClient(broker, clientId, persistence);
+			System.out.println("Connecting to broker: "+ broker);
+			mqttclient.connect(connOpts);
+
+		} catch (MqttException e) {
+			System.out.println("reason "+e.getReasonCode());
+			System.out.println("msg "+e.getMessage());
+			System.out.println("loc "+e.getLocalizedMessage());
+			System.out.println("cause "+e.getCause());
+			System.out.println("excep "+e);
+			e.printStackTrace();
+		}
+
+	}
+
+	public void Notify(String Topic, String message){
+		try {
+
+			MqttMessage mqttmessage = new MqttMessage();
+			mqttmessage.setPayload(message.getBytes());
+			mqttclient.publish(Topic, mqttmessage);
+			System.out.println("Message published to topic - " +  Topic);
+		} catch(MqttException me) {
+			System.out.println("reason "+me.getReasonCode());
+			System.out.println("msg "+me.getMessage());
+			System.out.println("loc "+me.getLocalizedMessage());
+			System.out.println("cause "+me.getCause());
+			System.out.println("excep "+me);
+			me.printStackTrace();
+		}
+	}
+
+	public void disconnect(){
+		try {
+			mqttclient.disconnect();
+		} catch (MqttException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
