78ae79418abcaa02fee5cc37a8a44c75d009770a
gbarrerassanz <gbarreras93@gmail.com>
2015-03-17 16:07:08 +0100

    Añadida la clase transformer.

--- GuiaTV/src/main/java/xmltv/transformer/XMLTVTransformer.java
+++ GuiaTV/src/main/java/xmltv/transformer/XMLTVTransformer.java
@@ -1,5 +1,21 @@
 package xmltv.transformer;
 
-public class XMLTVTransformer {
+import java.util.Iterator;
+
+import org.apache.commons.jxpath.JXPathContext;
+import org.springframework.integration.transformer.Transformer;
+import org.springframework.messaging.Message;
+
+public class XMLTVTransformer implements Transformer {
+
+	@Override
+	public Message<?> transform(Message<?> message) {
+		JXPathContext context = JXPathContext.newContext(message.getPayload());
+		Iterator<?> itProgrammes = context.iterate("/tv/programme");
+		while (itProgrammes.hasNext()) {
+			System.out.println(itProgrammes.next());
+		}
+		return null;
+	}
 
 }
